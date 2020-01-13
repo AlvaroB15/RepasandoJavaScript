93aafa2b0a343faeaefe5c29872eecc7da5706d7
Alvaro Begazo Carhuayo <alvaro15unmsm@gmail.com>
2020-01-12 21:16:27 -0500

    terminando objetos y entrando al DOM

--- usos.js
+++ usos.js
@@ -82,23 +82,111 @@ function objetos2(){
     text3.innerText = p1.listar();
 }
 
-/* function objetos3{
-    
-    // "instanciar objetos de la clase"
-    // sintaxis literal
+function objetos3(){
+    // PRIMERA forma de inicializar un Arreglo en JS
+    // var nombre = new Array("Alvaro","Paolo","Antony");
 
-    var p1 = {name}
 
+    // SEGUNDA forma de inicializar un Arreglo en JS
+    // var nombre = new Array(2);
+
+    // TERCERA forma de inicializar un Arreglo en JS
+    // var nombre = new Array();
+
+    // CUARTA forma de inicializar un Arreglo en JS
+    // esta es la forma mas recomendad para declarar
+    var nombre = ["Alvaro","Paolo","Anthony"]
+
+    nombre[0] = "Alvaro";
+    nombre[1] = "Paolo";
+    nombre[2] = "Anthony";
+
+    //
 
     var text1 = document.getElementById("parr1");
-    text1.innerText = p1.colorFav;
+    text1.innerText = nombre[0];
 
+    // Funciones de arreglos (length, concat)
     var text2 = document.getElementById("parr2");
-    text2.innerText = p2.colorFav;
-} */
+    text2.innerText = nombre.length; // no poner el () al length, osea no se debe poner lngth()
+
+    var colores = ["rojo", "verde","azul"];
+
+    var mixto = nombre.concat(colores);
+    var text3 = document.getElementById("parr3");
+    text3.innerText = mixto[4];
+
+    // en JS no existe matrices asociativas, pero algo parecido que se puede usar es: (hay que recordar que una matriz asociativa, es cuando se puede usar indices con nombres en vez de numeros )
+    var animales = [];
+    animales["nombre"] = "gringa";
+    animales["edad"] = 10;
+    var text4 = document.getElementById("parr4");
+    // si ponemos un indice como numero, me sale que el arreglo esta vacio osea undefined
+    text4.innerText = animales["nombre"];
+
+}
+
+function objetos4(){
+    /* los objetos con variable globales o constantes (mas), tenemos por ejemplo nombraremos a algunos:
+        E       => constante de Euler
+        LN2     => Logaritmo Natural 
+        LN10    => 
+        LOG2E   => 
+        LOG10E  => 
+        PI      => 
+    */
+
+    /* Algunas de las funciones "estaticas" ,algunos :
+            abs(x)
+            acos(x)
+            max(x,y,z,...)
+            min(x,y,z,...)
+            pow(x,y)
+            sqrt(x,y)
+            random()
+
+        La forma de usar es:
+            Math.sqrt(16);
+    */
+
+    var num = prompt("Ingrese numero para potencia: ");
+    alert("La potencia de "+num+" es: "+Math.pow(num,2));
+
+
+}
+
+function objetos5(){
+    // alert("Holi xddd");
+
+    /* Los metodos del objeto Date:
+        getFullYear()       => 
+        getMonth()          => 
+        getDate()           => 
+        getHours()          => 
+        getMinutes()        => 
+        getSeconds()        => 
+        getMilliseconds()   => 
+    */
+    var fecha = new Date();
+
+   var text1 = document.getElementById("parr1");
+   text1.innerText = fecha.getHours();
+
+   // Funciones de arreglos (length, concat)
+   var text2 = document.getElementById("parr2");
+   text2.innerText = fecha.getMinutes(); // no poner el () al length, osea no se debe poner lngth()
+
+   var text3 = document.getElementById("parr3");
+   text3.innerText = fecha.getSeconds();  
+}
 
 // Hace que se cargue la funcion que llamemos, al solo recargar o entrar por primera vez a la web
 
 // window.onload = cambio;
 // window.onload = tiposAlerta;
-window.onload = objetos2;
+// window.onload = objetos4;
+
+/* El setInterval, dice q a cada cierto tiempo se vuelve
+   a repetir la misma funcion */
+// window.onload = setInterval(objetos5,3000);
+window.onload = objetos5;
